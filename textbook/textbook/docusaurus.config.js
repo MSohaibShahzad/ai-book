6c95d4abbb2742bdc71bf704451aa4dc68870c2d
MSohaibShahzad <160708298+MSohaibShahzad@users.noreply.github.com>
2025-12-10 21:44:04 +0500

    Update docusaurus.config.js

--- textbook/textbook/docusaurus.config.js
+++ textbook/textbook/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://physical-ai-textbook.dev',
+  url: 'https://MSohaibShahzad.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ai-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
