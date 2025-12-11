1ffe9905818670f3881d0bea73010c8517470c95
MSohaibShahzad <160708298+MSohaibShahzad@users.noreply.github.com>
2025-12-11 22:50:11 +0500

    Update docusaurus.config.js

--- textbook/textbook/docusaurus.config.js
+++ textbook/textbook/docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/MSohaibShahzad/ai-book',
+            'https://github.com/MSohaibShahzad/ai-book/textbook/textbook',
         },
         blog: false,
         theme: {
